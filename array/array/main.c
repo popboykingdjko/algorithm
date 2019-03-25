279723d405d4959057d0a23cea12698e466c243c
popboy <kingdjko@tju.edu.cn>
2019-03-25 10:14:25 +0800

    提交array

---
+++ array/array/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  array
+//
+//  Created by pop boy on 2019/3/25.
+//  Copyright © 2019年 pop boy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
